152e96e98b9051202a327b33ec520ace43aaca8c
Szymon Kowal <133916679+szymon-kowal@users.noreply.github.com>
2023-07-17 19:27:12 +0200

    Time: 78 ms, Memory: 44.5 MB - LeetHub

---
+++ valid-palindrome/valid-palindrome.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    s = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+    for (let i = 0; i < s.length/2; i++) {
+        if (s[i] != s[s.length - 1 - i]){
+            return false;
+        }
+    }
+    return true;
+};
