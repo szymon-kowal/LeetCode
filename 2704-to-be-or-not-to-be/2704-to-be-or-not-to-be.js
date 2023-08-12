b656e85f84f4250b2667598b1d5aa9b771a1fb82
Szymon Kowal <133916679+szymon-kowal@users.noreply.github.com>
2023-08-12 19:19:09 +0200

    Time: 52 ms (75.96%), Space: 41.9 MB (48.86%) - LeetHub

---
+++ 2704-to-be-or-not-to-be/2704-to-be-or-not-to-be.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} val
+ * @return {Object}
+ */
+var expect = function(val) {
+    const toBe = (anotherVal) => {
+        if (val === anotherVal) {
+            return true;
+        } else {
+            throw new Error("Not Equal");
+        }
+    }
+    const notToBe = (anotherVal) => {
+        if (val !== anotherVal) {
+            return true;
+        } else {
+            throw new Error("Equal");
+        }
+    }
+    return {toBe, notToBe}
+};
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
