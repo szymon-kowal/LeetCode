1dbaec7fc8db726d3fff13175810c8091605de04
Szymon Kowal <133916679+szymon-kowal@users.noreply.github.com>
2023-08-25 16:39:02 +0200

    Time: 52 ms (70.99%), Space: 42.5 MB (74.95%) - LeetHub

---
+++ 2727-is-object-empty/2727-is-object-empty.js
@@ -0,0 +1,10 @@
+/**
+ * @param {Object | Array} obj
+ * @return {boolean}
+ */
+var isEmpty = function(obj) {
+    if (JSON.stringify(obj).length <= 2) 
+        return true
+    else 
+        return false;
+};
