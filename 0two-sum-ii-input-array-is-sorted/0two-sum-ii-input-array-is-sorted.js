de5ea83db939247cb3a6560831e4d932a4339f1f
Szymon Kowal <133916679+szymon-kowal@users.noreply.github.com>
2023-09-24 10:00:17 +0200

    Time: 48 ms, Memory: 42.9 MB - LeetHub

--- 0two-sum-ii-input-array-is-sorted/0two-sum-ii-input-array-is-sorted.js
+++ 0two-sum-ii-input-array-is-sorted/0two-sum-ii-input-array-is-sorted.js
@@ -5,11 +5,23 @@
  */
 var twoSum = function(numbers, target) {
 
-    for (let i = 0; i < numbers.length; i++) {
+    /* for (let i = 0; i < numbers.length; i++) {
         for (let j = i + 1; j < numbers.length; j++) {
             if (numbers[i] + numbers[j] === target) {
                 return [i+1, j+1];
             }
         }
     }
+    */
+    
+    let left = 0;
+    let right = numbers.length - 1;
+    while (numbers[left] + numbers[right] !== target) {
+        if (numbers[left] + numbers[right] > target) {
+            right--;
+        } else {
+            left++;
+        }
+    }
+    return [left + 1, right + 1]
 };
