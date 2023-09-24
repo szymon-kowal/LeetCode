779bffa243109ae09529d8847d8e352afc782c6e
Szymon Kowal <133916679+szymon-kowal@users.noreply.github.com>
2023-09-24 16:30:59 +0200

    Time: 57 ms, Memory: 46.3 MB - LeetHub

---
+++ minimum-size-subarray-sum/minimum-size-subarray-sum.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number} target
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minSubArrayLen = function(target, nums) {
+    let start = 0;
+    let end = 0;
+    let sum = nums[0];
+    let min_length = Infinity;
+    while (start <= end && end < nums.length){
+        if (sum >= target) {
+            min_length = Math.min(min_length, end-start+1);
+            sum -= nums[start];
+            start++;
+        } else {
+            end++;
+            sum += nums[end];
+        }
+    }
+    return min_length === Infinity ? 0 : min_length;
+    
+
+};
