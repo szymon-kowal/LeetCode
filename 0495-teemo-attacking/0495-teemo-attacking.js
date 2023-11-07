8274a99de40d5b282945da3051f883c318d3f493
Szymon Kowal <133916679+szymon-kowal@users.noreply.github.com>
2023-11-07 20:36:59 +0100

    Time: 52 ms (88.74%), Space: 45.8 MB (22.52%) - LeetHub

---
+++ 0495-teemo-attacking/0495-teemo-attacking.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} timeSeries
+ * @param {number} duration
+ * @return {number}
+ */
+var findPoisonedDuration = function(timeSeries, duration) {
+    let n = timeSeries.length;
+    if (n == 0) return 0;
+    let total = 0;
+    for (let i = 0; i < n - 1; i++) {
+        total += Math.min(timeSeries[i + 1] - timeSeries[i], duration);
+    }
+    return total + duration;
+};
