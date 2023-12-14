9577e3c7f8a34c86f20919268fb55ffd71fd30da
Szymon Kowal <133916679+szymon-kowal@users.noreply.github.com>
2023-12-14 22:34:51 +0100

    Time: 61 ms, Memory: 44.6 MB - LeetHub

---
+++ clone-graph/clone-graph.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for Node.
+ * class Node {
+ *     val: number
+ *     neighbors: Node[]
+ *     constructor(val?: number, neighbors?: Node[]) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.neighbors = (neighbors===undefined ? [] : neighbors)
+ *     }
+ * }
+ */
+
+const nodeMap = new Map<Node,Node>();
+
+function cloneGraph(node: Node | null): Node | null {
+	if (!node) return null;
+    
+    if (nodeMap.has(node)) {
+        return nodeMap.get(node)!;
+    }
+    
+    const newNode = new Node(node.val);
+    
+    nodeMap.set(node, newNode)
+    newNode.neighbors = node.neighbors.map((nei) => cloneGraph(nei)!)
+    
+    return newNode;
+};
