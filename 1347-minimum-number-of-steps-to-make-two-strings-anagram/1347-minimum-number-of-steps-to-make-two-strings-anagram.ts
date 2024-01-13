460b2e11933ab513c48b83b68d700cb9ed052e74
Szymon Kowal <133916679+szymon-kowal@users.noreply.github.com>
2024-01-13 09:55:19 +0100

    Time: 161 ms (11.43%), Space: 50.7 MB (25.71%) - LeetHub

---
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.ts
@@ -0,0 +1,17 @@
+function minSteps(s: string, t: string): number {
+    const sMap:Map<string, number> = new Map();
+    const tMap:Map<string, number> = new Map();
+    for (let i = 0; i < s.length; i++) {
+        sMap.set(s[i],sMap.get(s[i]) ? sMap.get(s[i]) + 1 : 1)
+        tMap.set(t[i],tMap.get(t[i]) ? tMap.get(t[i]) + 1 : 1)
+    }
+    let count:number = 0;
+    console.log(sMap)
+    for (const [char, freq] of tMap) {
+        const sFreq = sMap.get(char) || 0;
+            if (freq > sFreq) {
+                count += freq - sFreq;
+            }
+        }
+    return count;
+};
