9c78be53c47bd9ac96b237b5d340458088073a2b
Szymon Kowal <133916679+szymon-kowal@users.noreply.github.com>
2024-02-21 08:55:06 +0100

    Time: 255 ms (71.43%), Space: 72.9 MB (79.37%) - LeetHub

--- 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cs
+++ 2545-sort-the-students-by-their-kth-score/2545-sort-the-students-by-their-kth-score.cs
@@ -1,6 +1,6 @@
 public class Solution {
     public int[][] SortTheStudents(int[][] score, int k) {
-        Array.Sort(score, (a, b) => b[k].CompareTo(a[k]));
+        Array.Sort(score, (a, b) => b[k] - a[k]);
         return score;
     }
 }
