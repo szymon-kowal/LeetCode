e5a7088c1b5a723375645eefefce841b2b6d511a
Szymon Kowal <133916679+szymon-kowal@users.noreply.github.com>
2024-03-11 08:39:19 +0100

    Time: 44 ms (63.33%), Space: 37.1 MB (88.33%) - LeetHub

--- 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cs
+++ 1876-substrings-of-size-three-with-distinct-characters/1876-substrings-of-size-three-with-distinct-characters.cs
@@ -2,9 +2,10 @@ public class Solution {
     public int CountGoodSubstrings(string s) {
         var count = 0;
 
-        for (int i = 0; i < s.Length - 2; i++) {
-            if (s.Substring(i, 3).ToHashSet().Count == 3)
+        for (int i = 1; i < s.Length - 1; i++) {
+            if (s[i - 1] != s[i] && s[i] != s[i + 1] && s[i + 1] != s[i - 1]) {
                 count++;
+            }
         }
 
         return count;
