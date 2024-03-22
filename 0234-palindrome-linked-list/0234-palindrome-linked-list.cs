940af7bf181ee1b1c7d6a4b31be240f071a27317
Szymon Kowal <133916679+szymon-kowal@users.noreply.github.com>
2024-03-22 11:44:34 +0100

    Time: 497 ms (5.01%), Space: 72.5 MB (22.56%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cs
@@ -0,0 +1,41 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool IsPalindrome(ListNode head) {
+        if (head.next == null) return true;
+        Stack<int> stackOfVals = new();
+        ListNode slow = head;
+        ListNode fast = head;
+        
+        stackOfVals.Push(slow.val);
+        
+        while (fast.next is not null && fast.next.next is not null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            stackOfVals.Push(slow.val);
+        }
+        
+        if (fast.next != null) {
+            slow = slow.next;
+        }
+        foreach(var val in stackOfVals) {
+            Console.WriteLine(val);
+        }
+        while (slow != null) {
+            int top = stackOfVals.Pop();
+            if (top != slow.val) return false;
+            slow = slow.next;
+        }
+            
+        return true;
+    }
+}
