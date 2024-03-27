1a825a2763bb0b2f4cdd021e8a10ac79d84783fc
Szymon Kowal <133916679+szymon-kowal@users.noreply.github.com>
2024-03-27 10:52:14 +0100

    Time: 69 ms (47.47%), Space: 41.4 MB (50.88%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cs
+++ 0162-find-peak-element/0162-find-peak-element.cs
@@ -2,11 +2,11 @@ public class Solution {
     public int FindPeakElement(int[] nums) {
         int left = 0;
         int right = nums.Length - 1;
-        while (left + 1 < right) {
+        while (left < right) {
             int mid = left + (right - left) / 2;
             
             if (nums[mid] < nums[mid + 1]) {
-                left = mid;
+                left = mid + 1;
             }
             else {
                 right = mid;
