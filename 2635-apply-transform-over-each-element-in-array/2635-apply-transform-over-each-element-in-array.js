46a6682bf1a50de7aaa48d4dd8a9ad518e19e6ca
Szymon Kowal <133916679+szymon-kowal@users.noreply.github.com>
2024-04-28 18:57:40 +0200

    Time: 49 ms (70.27%), Space: 47.5 MB (99.65%) - LeetHub

--- 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
+++ 2635-apply-transform-over-each-element-in-array/2635-apply-transform-over-each-element-in-array.js
@@ -4,9 +4,8 @@
  * @return {number[]}
  */
 var map = function(arr, fn) {
-    let newArray = [];
     for (let i = 0; i < arr.length; i++) {
-        newArray[i] = fn(arr[i],i)
+        arr[i] = fn(arr[i], i);
     }
-    return newArray;
+    return arr;
 };
