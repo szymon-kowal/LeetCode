66f759cbb59c54be64350c46e3eaf34bda1aa7a0
Szymon Kowal <133916679+szymon-kowal@users.noreply.github.com>
2024-04-28 20:03:22 +0200

    Time: 53 ms (95.58%), Space: 49.2 MB (75.22%) - LeetHub

--- 2715-timeout-cancellation/2715-timeout-cancellation.js
+++ 2715-timeout-cancellation/2715-timeout-cancellation.js
@@ -5,14 +5,13 @@
  * @return {Function}
  */
 var cancellable = function(fn, args, t) {
-    // cancelFn function//
-    const cancelFn = function (){
+  const timer = setTimeout(() => {
+    fn(...args)
+  }, t)
+  const cancelFn = function() {
       clearTimeout(timer);
-  };
-  const timer = setTimeout(()=>{
-      fn(...args)
-  }, t);
-  return cancelFn ;
+  } 
+  return cancelFn;
 };
 
 /**
